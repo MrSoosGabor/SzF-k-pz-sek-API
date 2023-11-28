6d63996ec65d525bdb46f770ead7a4c44a01f160
Soós Gábor <soos.gabor@jedlik.eu>
2023-11-28 09:14:56 +0100

    Saját hibaosztály létrehozva

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 const express = require('express')
+const trainings = require('./routes/trainings')
+const errorHandler = require('./middleware/error')
+
 require('dotenv').config() // A .env fájlt olvassa
 const app = express()
-app.listen(process.env.PORT, ()=> console.log(`Server running on port ${process.env.PORT}`));
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+database.on("error", (error) => {
+    console.log(error);
+});
+database.once("connected", () => {
+    console.log(`Database Connected ${database.host}`);
+});
+
+app.use(express.json())
+
+const morgan = require('morgan')
+
+app.use(morgan('dev'))
+
+app.use('/api/trainings', trainings)
+app.use(errorHandler)
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
